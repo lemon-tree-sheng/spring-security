80e9b7a0292798d99fcf1a29fcb3f13e77f38dfa
shengxingyue <shengxingyue@souche.com>
2017-10-18 02:07:48 +0800

    hello spring security

---
+++ spring-security-demo/src/main/java/com/sheng/security/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.sheng.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by shengxingyue on 2017/10/18.
+ */
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello spring security";
+    }
+}
