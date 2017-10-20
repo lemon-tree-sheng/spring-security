f958c5b218a270064273efce26feaed4f034b449
shengxingyue <shengxingyue@souche.com>
2017-10-21 00:51:30 +0800

    spring boot 对于异步的配置支持

--- spring-security-demo/src/main/java/com/sheng/security/config/WebConfig.java
+++ spring-security-demo/src/main/java/com/sheng/security/config/WebConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -20,6 +21,15 @@ public class WebConfig extends WebMvcConfigurerAdapter{
 
     @Autowired
     private TimeInterceptor timeInterceptor;
+
+    // spring boot 对于异步的配置支持
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+//        configurer.registerCallableInterceptors()
+//        configurer.registerDeferredResultInterceptors()
+//        configurer.setDefaultTimeout()
+//        configurer.setTaskExecutor()
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
